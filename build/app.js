5df0198a7acc0b7c4d6154219ab54dc6148165c9
Extenv <im.ivan.maulana@gmail.com>
2022-06-11 06:15:51 +0700

    learn typescript

--- build/app.js
+++ build/app.js
@@ -1,16 +1,36 @@
 "use strict";
-//Basic typescript
+// Language: typescript
+// Path: src/app.ts
 let myname = 'Ivan Maulana';
-console.log(myname); //Ivan Maulana
+//console.log(myname); //Ivan Maulana
 function basicFunc(name) {
     return `name: ${name}`;
 }
-console.log(basicFunc('Ivan Maulana')); //name: Ivan Maulana
+//console.log(basicFunc('Ivan Maulana')); //name: Ivan Maulana
 function opsiFunc(name) {
     return `name: ${name}`;
 }
-console.log(opsiFunc()); //name: undefined
+//console.log(opsiFunc()); //name: undefined
 function customFunc(name = "Wajib diisi !!!") {
     return `name: ${name}`;
 }
-console.log(customFunc()); //name: Wajib diisi !!!
+function myBio(fill) {
+    console.log(`${fill.nama}, ${fill.tinggi}`);
+}
+const mybio = { nama: "Ivan Maulana", tinggi: 170 };
+//myBio(mybio); //Ivan Maulana, 170
+class VirtualBio {
+    constructor(nama, tinggi) {
+        this.nama = nama;
+        this.tinggi = tinggi;
+    }
+}
+const newBio = new VirtualBio("Extenv", 170);
+//myBio(newBio); //Ivan Maulana, 170
+function cetak(mencetak) {
+    console.log(mencetak);
+}
+let array = [1, 2, 3, 4, 5];
+for (let get in array) {
+    cetak(get);
+}
--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,59 @@
-//Basic typescript
+
+// Language: typescript
+// Path: src/app.ts
+
 
 let myname: string = 'Ivan Maulana';
-console.log(myname); //Ivan Maulana
+//console.log(myname); //Ivan Maulana
 
 
 function basicFunc(name: String): string {
     return `name: ${name}`;
 }
-console.log(basicFunc('Ivan Maulana')); //name: Ivan Maulana
+//console.log(basicFunc('Ivan Maulana')); //name: Ivan Maulana
 
 
 function opsiFunc(name?: String): string {
     return `name: ${name}`;
 }
-console.log(opsiFunc()); //name: undefined
+//console.log(opsiFunc()); //name: undefined
 
 
 function customFunc(name: String = "Wajib diisi !!!"): string {
     return `name: ${name}`;
 }
-console.log(customFunc()); //name: Wajib diisi !!!
+//console.log(customFunc()); //name: Wajib diisi !!!
+
+interface Bio {
+    nama: string;
+    tinggi: number;
+  }
+   
+function myBio(fill: Bio) {
+    console.log(`${fill.nama}, ${fill.tinggi}`);
+}
+
+const mybio = { nama: "Ivan Maulana", tinggi: 170 };
+//myBio(mybio); //Ivan Maulana, 170
+
+
+class VirtualBio {
+    nama: string;
+    tinggi: number;
+    constructor(nama: string, tinggi: number) {
+      this.nama = nama;
+      this.tinggi = tinggi;
+    }
+  }
+
+const newBio = new VirtualBio("Extenv", 170);
+//myBio(newBio); //Ivan Maulana, 170
+
+function cetak(mencetak?:any){
+   return console.log(mencetak);
+}
+
+let array = [1,2,3,4,5];
+for(let get in array){
+    //cetak(get) 
+}
